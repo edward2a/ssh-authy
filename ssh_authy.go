7c18625c21d432b91f6c90266330d5e77e6dd19e
Eduardo A. Paris Penas <edward2a@gmail.com>
2017-07-26 01:47:54 +0100

    added single arg input with validation for username provided by ssh; added user-data http request for platform info

--- ssh_authy.go
+++ ssh_authy.go
@@ -2,10 +2,12 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
   "log"
-  //"net/http"
+  "net/http"
   "os"
   "strings"
+  "time"
   "github.com/aws/aws-sdk-go/aws"
   //"github.com/aws/aws-sdk-go/aws/awserr"
   "github.com/aws/aws-sdk-go/aws/session"
@@ -31,13 +33,40 @@ type platform struct {
 
 
 func validate_input() {
-  return true
+  if len(os.Args) == 2 {
+    if allowed_users[os.Args[1]] { ; } else { os.Exit(1) }
+  } else {
+    os.Exit(1)
+  }
 }
 
 // Retrieve user-data (expecting project and env name here)
 func get_project_info() platform {
   // temporarily return static data for testing
-  return platform{"myProject", "dev"}
+  // return platform{"myProject", "dev"}
+
+  var prj string
+  var env string
+
+  client :=  &http.Client{ Timeout: time.Second * 5 }
+  req, _ := http.NewRequest("GET", user_data_url, nil)
+
+  resp, err := client.Do(req)
+  if err != nil { log.Fatal("Failed to retrieve platform info") }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil { log.Fatal("Failed parsing platform info") }
+
+  vars := strings.Split(string(body), "\n")
+  for i:=1; i < len(vars); i++ {
+    if strings.HasPrefix(vars[i], "ProjectName=") {
+      prj = strings.TrimPrefix(vars[i], "ProjectName=")
+    } else if strings.HasPrefix(vars[i], "Environment=") {
+      env = strings.TrimPrefix(vars[i], "Environment=")
+    }
+  }
+
+  return platform{prj, env}
 }
 
 // Instantiate an S3 client
@@ -96,6 +125,7 @@ func get_keys(bkt string, path string, users []string, client *s3.S3) [][]byte {
 
 
 func main() {
+  validate_input()
   s3_client := get_client()
   project_data := get_project_info()
   user_list := list_users(bucket, project_data, s3_client)
